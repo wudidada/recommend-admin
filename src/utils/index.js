99120e191baac096f1fa4a466555a2f8fee6acfe
jusbin <hackerjiang@gmail.com>
2021-04-27 22:47:44 +0800

    fix some problems

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,16 +1,16 @@
-import { resetRouter } from '@/router'
+import { resetRouter } from "@/router";
 
 export function resetTokenAndClearUser() {
-    // 退出登陆 清除用户资料
-    localStorage.setItem('token', '')
-    localStorage.setItem('userImg', '')
-    localStorage.setItem('userName', '')
-    // 重设路由
-    resetRouter()
+  // 退出登陆 清除用户资料
+  localStorage.setItem("token", "");
+  localStorage.setItem("userImg", "");
+  localStorage.setItem("userName", "");
+  // 重设路由
+  resetRouter();
 }
 
-export const defaultDocumentTitle = 'vue-admin-template'
+export const defaultDocumentTitle = "电影推荐后台";
 export function getDocumentTitle(pageTitle) {
-    if (pageTitle) return `${defaultDocumentTitle} - ${pageTitle}`
-    return `${defaultDocumentTitle}`
+  if (pageTitle) return `${defaultDocumentTitle} - ${pageTitle}`;
+  return `${defaultDocumentTitle}`;
 }
